68d08a95f5da8f3c6d40154bd7466a8e3eb921bf
Romaric Sieyamdji <98168160+romaric111@users.noreply.github.com>
2025-06-11 07:12:17 -0400

    fichier pour update la classe produit

---
+++ UpdateProduitDto.cs
@@ -0,0 +1,13 @@
+﻿namespace App_produit.Models.Entities
+{
+    public class UpdateProduitDto
+    {
+
+        
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+        public required decimal Price { get; set; }
+        public required DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Le temps par defaut est UTC
+    }
+}
