9d452ee24f1cbf435d1f112186cad3b78ef3e3be
Romaric Sieyamdji <98168160+romaric111@users.noreply.github.com>
2025-06-11 07:00:53 -0400

    creation de la classe produit

---
+++ Produit.cs
@@ -0,0 +1,17 @@
+﻿namespace App_produit.Models.Entities
+{
+    public class Produit
+    // Properties of the Produit class: Id (int)
+/* Name(string) – obligatoire
+ Description(string)
+ Price(decimal) – obligatoire
+ CreatedAt(DateTime) – obligatoire*/
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; } 
+
+        public string? Description { get; set; }
+        public required decimal Price { get; set; }
+        public required DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Le temps par defaut est UTC
+    }
+}
