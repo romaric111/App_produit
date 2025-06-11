24e58812b38d3af40ea495539322f8c1a3af18f6
Romaric Sieyamdji <98168160+romaric111@users.noreply.github.com>
2025-06-11 07:15:12 -0400

    fichier pour la BD

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using App_produit.Models.Entities;
+using Microsoft.EntityFrameworkCore; 
+
+namespace App_produit.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Produit> Produits { get; set; }
+
+    }
+    
+    
+}
