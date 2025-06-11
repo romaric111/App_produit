d5c9887bcced7f6629f65d23fd7f86f2c7763468
Romaric Sieyamdji <98168160+romaric111@users.noreply.github.com>
2025-06-11 06:59:50 -0400

    model class produit utiliser lors du test ajout

---
+++ AddProduitDto.cs
@@ -0,0 +1,12 @@
+﻿namespace App_produit.Models
+{
+    public class AddProduitDto
+    {
+        
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+        public required decimal Price { get; set; }
+        public required DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Le temps par defaut est UTC
+    }
+}
